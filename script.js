dbb342dcccd6ba5895abf3152d3448fa23146cda
grace halliday <gracehally7@gmail.com>
2022-08-07 01:00:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let menu = document.querySelector("#plus");
+let paragraph= document.querySelector('items');
+
+//add event listener
+menu.onclick = {} =>{
+    menu.classList.toggle('items_span')
+
+   
+    }
+    menu.classList.remove('items_span')
